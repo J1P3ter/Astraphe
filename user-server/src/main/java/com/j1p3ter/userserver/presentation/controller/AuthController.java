5b6471443cff429a1a89c3fcb7fbc8eeb2043813
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-10-04 10:50:31 +0900

    fix: change from '/api/user' to '/api/users' (#2)

--- user-server/src/main/java/com/j1p3ter/userserver/presentation/controller/AuthController.java
+++ user-server/src/main/java/com/j1p3ter/userserver/presentation/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class AuthController {
 
     private final UserService userService;
