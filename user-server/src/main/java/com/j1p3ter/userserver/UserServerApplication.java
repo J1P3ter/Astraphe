a79083d2b6d6f1945859ec15c22bc00dcfb8cfc4
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-10-05 15:25:51 +0900

    feat: add @EnableDiscoveryClient (#2)

--- user-server/src/main/java/com/j1p3ter/userserver/UserServerApplication.java
+++ user-server/src/main/java/com/j1p3ter/userserver/UserServerApplication.java
@@ -2,6 +2,7 @@ package com.j1p3ter.userserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 "com.j1p3ter.common"
         }
 )
+@EnableDiscoveryClient
 public class UserServerApplication {
 
     public static void main(String[] args) {
