5f2a7c871229c091429c029ca17d835c59ccc69a
lukeydokey <pudingles@gmail.com>
2024-10-06 18:32:50 +0900

    feat: Product, Category Entity 생성 (#20)

---
+++ product-server/src/main/java/com/j1p3ter/productserver/domain/product/Product.java
@@ -0,0 +1,68 @@
+package com.j1p3ter.productserver.domain.product;
+
+import com.j1p3ter.common.auditing.BaseEntity;
+import com.j1p3ter.productserver.domain.company.Company;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@Entity
+@SQLRestriction("is_delete is false")
+@Table(name = "tb_products")
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+
+    @Column(name = "product_name", nullable = false, length = 50)
+    private String productName;
+
+    @Column(name = "product_image_url")
+    private String productImgUrl;
+
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    @Column(name = "description_image_url")
+    private String descriptionImgUrl;
+
+    @Column(name = "original_price", nullable = false)
+    private Integer originalPrice;
+
+    @Column(name = "discounted_price", nullable = false)
+    private Integer discountedPrice;
+
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
+
+    @OneToOne
+    @JoinColumn(name = "category_code", nullable = false)
+    private Category category;
+
+    @Column(name = "sale_start_time", nullable = false)
+    private LocalDateTime saleStartTime;
+
+    @Column(name = "sale_end_time", nullable = false)
+    private LocalDateTime saleEndTime;
+
+    @Column(name = "rate", nullable = false)
+    private Double rate = 0.0;
+
+    @Column(name = "is_soldout", nullable = false)
+    private Boolean isSoldout = false;
+
+    @Column(name = "is_hidden", nullable = false)
+    private Boolean isHidden = false;
+}
