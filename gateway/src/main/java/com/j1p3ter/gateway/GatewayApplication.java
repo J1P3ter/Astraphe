2a43e2c5d6fab160037a53cb0c73d7002a1624de
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-10-06 14:39:04 +0900

    refactor: add common dependency into gateway (#8)

--- gateway/src/main/java/com/j1p3ter/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/j1p3ter/gateway/GatewayApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
 		scanBasePackages = {
-				"com.j1p3ter.gateway"
+				"com.j1p3ter.gateway",
+				"com.j1p3ter.common.config"
 		}
 )
 public class GatewayApplication {
