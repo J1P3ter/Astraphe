20f1e68ed8e0969ec2220e84ddd8bdbf9760f0c9
ayoung <ayoung3052@gmail.com>
2024-10-08 22:49:42 +0900

    refactor : Redis 타입 변경 / 직렬화 설정

--- queue-server/src/main/java/com/j1p3ter/queueserver/config/ReactiveRedisConfig.java
+++ queue-server/src/main/java/com/j1p3ter/queueserver/config/ReactiveRedisConfig.java
@@ -4,12 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class ReactiveRedisConfig {
     @Bean
-    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
-        return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.string());
+    public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        RedisSerializationContext.RedisSerializationContextBuilder<String, Object> builder =
+                RedisSerializationContext.newSerializationContext(RedisSerializer.string());
+        RedisSerializationContext<String, Object> context = builder.value(serializer).build();
+
+        return new ReactiveRedisTemplate<>(factory, context);
     }
 }
