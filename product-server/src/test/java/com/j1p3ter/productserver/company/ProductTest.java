3b97cc0166e58fe7da7a358df438684693d59985
송형근 <soo1095@naver.com>
2024-10-08 10:52:45 +0900

    feat: Product Search Test Code 작성 (#20)

--- product-server/src/test/java/com/j1p3ter/productserver/company/ProductTest.java
+++ product-server/src/test/java/com/j1p3ter/productserver/company/ProductTest.java
@@ -9,12 +9,17 @@ import com.j1p3ter.productserver.application.dto.product.ProductCreateRequestDto
 import com.j1p3ter.productserver.application.dto.product.ProductOptionDto;
 import com.j1p3ter.productserver.application.dto.product.ProductResponseDto;
 import com.j1p3ter.productserver.application.dto.product.ProductUpdateRequestDto;
+import com.j1p3ter.productserver.domain.company.Company;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
@@ -125,6 +130,104 @@ class ProductTest {
         assertThat(productResponseDto.getDescription()).isEqualTo("DescriptionP");
         assertThat(productResponseDto.getProductOptions().get(0).getOptionName()).isEqualTo("Blue");
 
+    }
+
+    @Test
+    @DisplayName("Search Product Test - Company Name")
+    void searchProductByCompanyNameTest(){
+        // Given
+        CompanyCreateRequestDto companyCreateRequestDto = new CompanyCreateRequestDto(
+                1L,
+                "SecondCompany",
+                "Description2",
+                "Address2"
+        );
+
+        CompanyResponseDto secondCompany = companyService.createCompany(companyCreateRequestDto);
+
+        ProductOptionDto productOptionDto = new ProductOptionDto(
+                "Black",
+                "Color",
+                1000
+        );
+
+        List<ProductOptionDto> optionList = new ArrayList<>();
+        optionList.add(productOptionDto);
+
+        ProductCreateRequestDto productCreateRequestDto = new ProductCreateRequestDto(
+                secondCompany.getId(),
+                "SecondProductNameforTest",
+                "DescriptionProduct",
+                10000,
+                8000,
+                100,
+                optionList,
+                3000L,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1L)
+        );
+
+        productService.createProduct(1L, productCreateRequestDto);
+
+        // When
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.fromString("DESC"), "createdAt"));
+        // Company Name Second를 포함하는 모든 카테고리 DESC createdAt
+        Page<ProductResponseDto> productResponseDtos = productService.searchProduct("Second", null, 0L, pageable);
+
+        // Then
+        assertThat(productResponseDtos.getContent().size()).isEqualTo(1);
+        assertThat(productResponseDtos.getContent().get(0).getCompanyName()).isEqualTo("SecondCompany");
+        assertThat(productResponseDtos.getContent().get(0).getProductName()).isEqualTo("SecondProductNameforTest");
+    }
+
+    @Test
+    @DisplayName("Search Product Test - Product Name")
+    void searchProductByProductNameTest(){
+        // Given
+        CompanyCreateRequestDto companyCreateRequestDto = new CompanyCreateRequestDto(
+                1L,
+                "SecondCompany",
+                "Description2",
+                "Address2"
+        );
+
+        CompanyResponseDto secondCompany = companyService.createCompany(companyCreateRequestDto);
+
+        ProductOptionDto productOptionDto = new ProductOptionDto(
+                "Black",
+                "Color",
+                1000
+        );
+
+        List<ProductOptionDto> optionList = new ArrayList<>();
+        optionList.add(productOptionDto);
+
+        ProductCreateRequestDto productCreateRequestDto = new ProductCreateRequestDto(
+                secondCompany.getId(),
+                "SecondProductNameforTest",
+                "DescriptionProduct",
+                10000,
+                8000,
+                100,
+                optionList,
+                3000L,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1L)
+        );
+
+        productService.createProduct(1L, productCreateRequestDto);
+
+        // When
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.fromString("DESC"), "createdAt"));
+        // Product Name ProductNameforTest를 포함하는 모든 카테고리 DESC createdAt
+        Page<ProductResponseDto> productResponseDtos = productService.searchProduct(null, "ProductNameforTest", 0L, pageable);
+
+        // Then
+        assertThat(productResponseDtos.getContent().size()).isEqualTo(2);
+        assertThat(productResponseDtos.getContent().get(0).getCompanyName()).isEqualTo("SecondCompany");
+        assertThat(productResponseDtos.getContent().get(1).getCompanyName()).isEqualTo("CompanyNameforTest");
+        assertThat(productResponseDtos.getContent().get(0).getProductName()).isEqualTo("SecondProductNameforTest");
+        assertThat(productResponseDtos.getContent().get(1).getProductName()).isEqualTo("ProductNameforTest");
     }
 
     @Test
