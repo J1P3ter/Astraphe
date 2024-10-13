91c80c7d2ee8eb4b8fc551ac75da590fec5d7999
lossol1 <thffo123@gmail.com>
2024-10-14 04:13:38 +0900

    fix : 사용 하지 않는 import 삭제 #5

--- order-server/src/main/java/com/j1p3ter/orderserver/domain/order/OrderRepository.java
+++ order-server/src/main/java/com/j1p3ter/orderserver/domain/order/OrderRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Page<Order> findAllByIsDeletedFalse(Pageable pageable);
 
