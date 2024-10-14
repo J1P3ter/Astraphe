f7e837cb840177130a12005c4b53385693372912
ayboori <105356296+ayboori@users.noreply.github.com>
2024-10-14 11:10:50 +0900

    fix : 대소문자 수정 (#12)

--- queue-server/src/main/java/com/j1p3ter/queueserver/application/dto/AllowResponseDto.java
+++ queue-server/src/main/java/com/j1p3ter/queueserver/application/dto/AllowResponseDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AllowResponseDto {
-    private Long RequestCount;
+    private Long requestCount;
     private Long allowedCount;
 
 }
