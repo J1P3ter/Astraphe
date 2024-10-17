8e3b651a98a19e83585e9a30cdd31cdd9143a092
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-10-17 10:12:05 +0900

    feat: add AOP

--- user-server/src/main/java/com/j1p3ter/userserver/presentation/controller/AuthController.java
+++ user-server/src/main/java/com/j1p3ter/userserver/presentation/controller/AuthController.java
@@ -6,17 +6,11 @@ import com.j1p3ter.userserver.application.service.UserService;
 import com.j1p3ter.userserver.presentation.request.LogInRequestDto;
 import com.j1p3ter.userserver.presentation.request.SignUpRequestDto;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Objects;
 
 
 @RestController
@@ -27,12 +21,7 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/signUp")
-    public ApiResponse<?> signUp(@Valid @RequestBody SignUpRequestDto signUpRequestDto,
-                                 BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return ApiResponse.error(HttpStatus.BAD_REQUEST.value(), Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage());
-        }
-
+    public ApiResponse<?> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
         try {
             return ApiResponse.success(userService.createUser(signUpRequestDto));
         } catch (ApiException e) {
@@ -41,13 +30,8 @@ public class AuthController {
     }
 
     @PostMapping("/logIn")
-    public ApiResponse<?> logIn(@Valid @RequestBody LogInRequestDto logInRequestDto,
-                                BindingResult bindingResult,
+    public ApiResponse<?> logIn(@RequestBody LogInRequestDto logInRequestDto,
                                 HttpServletResponse response) {
-        if (bindingResult.hasErrors()) {
-            return ApiResponse.error(HttpStatus.BAD_REQUEST.value(), Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage());
-        }
-
         try {
             return ApiResponse.success(userService.logIn(logInRequestDto, response));
         } catch (ApiException e) {
