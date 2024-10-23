bcd160492f78a6ce20701f4c19b38e3b88234bfa
ayboori <105356296+ayboori@users.noreply.github.com>
2024-10-24 02:49:03 +0900

    Reapply "feat : subscribeProduct CRD"
    This reverts commit 8925dfde9e1fba7284f80087cc0b5adff4deabf4.

---
+++ product-server/src/main/java/com/j1p3ter/productserver/application/dto/subscribeProduct/SubscribeProductResponseDto.java
@@ -0,0 +1,22 @@
+package com.j1p3ter.productserver.application.dto.subscribeProduct;
+
+import com.j1p3ter.productserver.domain.subscribe.SubscribeProduct;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscribeProductResponseDto {
+    private Long subscribeProductId;
+    private Long productId;
+    private Long userId;
+    public SubscribeProductResponseDto(SubscribeProduct subscribeProduct) {
+        this.subscribeProductId = subscribeProduct.getId();
+        this.productId = subscribeProduct.getProduct().getId();
+        this.userId = subscribeProduct.getUserId();
+    }
+}
---
+++ product-server/src/main/java/com/j1p3ter/productserver/domain/subscribe/SubscribeProductRepository.java
@@ -0,0 +1,6 @@
+package com.j1p3ter.productserver.domain.subscribe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribeProductRepository extends JpaRepository<SubscribeProduct,Long> {
+}
