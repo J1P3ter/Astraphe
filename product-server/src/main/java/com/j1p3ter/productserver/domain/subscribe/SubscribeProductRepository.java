8925dfde9e1fba7284f80087cc0b5adff4deabf4
ayboori <105356296+ayboori@users.noreply.github.com>
2024-10-24 02:48:02 +0900

    Revert "feat : subscribeProduct CRD"
    This reverts commit b1425646f07642832222854cf2eb5df9a5d8c603.

--- product-server/src/main/java/com/j1p3ter/productserver/domain/subscribe/SubscribeProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.j1p3ter.productserver.domain.subscribe;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscribeProductRepository extends JpaRepository<SubscribeProduct,Long> {
-}
